f18b89cb46769ac79b8457be9f8aaf7cfe7335e4
Garret <gwiats@gmail.com>
2021-03-21 14:48:33 -0400

    commit tree 57759c6aa2b45e09d7920e3454b00b7e17e3f54a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(x) {
+    let check = x.find(function(v) {
+        return v.result === "W";
+    })
+    if (typeof check === 'undefined') {
+        return check
+    } else {
+        return check.year
+    }   
+}
